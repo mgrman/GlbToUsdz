0f477bc7f5b1e980c3cd72612e445774d436d2d9
Martin Grman <martingrman@outlook.com>
2024-08-07 22:49:34 +0200

    cleanup

--- Program.cs
+++ Program.cs
@@ -1,85 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-using THREE;
-using THREEToUsdz;
-using static System.Formats.Asn1.AsnWriter;
+﻿
+using SharpGLTF.Schema2;
+using System.Globalization;
+using System.Numerics;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+// based on https://github.com/ft-lab/sample_usd/blob/main/readme.md
 
-var scene = new Scene();
+var model = SharpGLTF.Schema2.ModelRoot.Load("Duck.glb");
 
+var rootNoted = model.DefaultScene.VisualChildren;
 
-
-var planeGeometry = new PlaneBufferGeometry(60, 40, 1, 1);
-MeshLambertMaterial planeMaterial = new MeshLambertMaterial() { Color = Color.Hex(0xffffff) };
-
-var plane = new Mesh(planeGeometry, planeMaterial);
-plane.ReceiveShadow = true;
-
-plane.Rotation.X = (float)(-0.5 * System.Math.PI);
-plane.Position.X = 0;
-plane.Position.Y = 0;
-plane.Position.Z = 0;
-
-scene.Add(plane);
+var sb = new StringBuilder();
+sb.AppendLine("#usda 1.0");
 
 
-
-var vertices = new List<Vector3>(){
-                  new Vector3(1, 3, 1),
-                  new Vector3(1, 3, -1),
-                  new Vector3(1, -1, 1),
-                  new Vector3(1, -1, -1),
-                  new Vector3(-1, 3, -1),
-                  new Vector3(-1, 3, 1),
-                  new Vector3(-1, -1, -1),
-                  new Vector3(-1, -1, 1)
-             };
-
-var faces = new List<Face3>() {
-                new Face3(0, 2, 1),
-                new Face3(2, 3, 1),
-                new Face3(4, 6, 5),
-                new Face3(6, 7, 5),
-                new Face3(4, 5, 1),
-                new Face3(5, 0, 1),
-                new Face3(7, 6, 2),
-                new Face3(6, 3, 2),
-                new Face3(5, 7, 0),
-                new Face3(7, 2, 0),
-                new Face3(1, 3, 4),
-                new Face3(3, 6, 4),
-
-            };
-
-var controlPoint = new List<Vector3>(vertices);
-
-var geom = new Geometry();
-geom.Vertices = vertices;
-geom.Faces = faces;
-
-geom.ComputeFaceNormals();
-
-
-var materials = new List<Material>(){
-                    new MeshBasicMaterial() { Color = Color.ColorName(ColorKeywords.black),Wireframe=true},
-                    new MeshLambertMaterial() { Opacity=0.6f,Color = new Color().SetHex(0x44ff44),Transparent=true }
-
-                };
-
-var mesh = SceneUtils.CreateMultiMaterialObject(geom, materials);
-
-mesh.Traverse(o =>
+void Process(IVisualNodeContainer visualNode)
 {
-    o.CastShadow = true;
-});
-scene.Add(mesh);
+
+    if(visualNode is Node node)
+    {
+        if (node.Mesh != null)
+        {
+            foreach(var primitive in node.Mesh.Primitives)
+            {
+                if(primitive.DrawPrimitiveType == PrimitiveType.TRIANGLES)
+                {
+                    Matrix4x4.Decompose(node.WorldMatrix, out var scale, out var rotation, out var translation);
+
+                    var indices=primitive.GetIndices();
+
+                    var vertices = primitive.GetVertices("POSITION");
+                    var normal = primitive.GetVertices("NORMAL");
+                    var uv_0 = primitive.GetVertices("TEXCOORD_0");
+
+                    CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+
+                    sb.AppendLine($"");
+                    sb.AppendLine($"def Xform \"root\"");
+                    sb.AppendLine($"{{");
+                    sb.AppendLine($"    double3 xformOp:translate = ({translation.X:F3}, {translation.Y:F3}, {translation.Z:F3})");
+                    sb.AppendLine($"    uniform token[] xformOpOrder = [\"xformOp: translate\"]");
+                    sb.AppendLine($"");
+                    sb.AppendLine($"    def Mesh \"quad\"");
+                    sb.AppendLine($"    {{");
+                    sb.AppendLine($"        point3f[] points = [{string.Join(",", vertices.AsVector3Array().Select(p=>$"({p.X:F3}, {p.Y:F3}, {p.Z:F3})"))}]");//(0, 0, 0), (1, 0, 0), (1, 1, 0), (0, 1, 0)]");
+                    sb.AppendLine($"        int[] faceVertexIndices = [{string.Join(", ", indices)}]");
+                    sb.AppendLine($"        int[] faceVertexCounts = [{string.Join(", ",Enumerable.Repeat(3, indices.Count/3))}]");
+                    sb.AppendLine($"    }}");
+                    sb.AppendLine($"}}");
+                }
+            }
+            
+
+        }
+    }
 
 
 
+    foreach (var child in visualNode.VisualChildren)
+    {
+        Process(child);
+    }
 
-var bytes = await USDZExporter.ParseAsync(scene);
+}
 
 
-File.WriteAllBytes(@"C:\Users\marti\OneDrive\Desktop\test.usdz", bytes);
+Process(model.DefaultScene);
 
-Console.WriteLine(bytes.Length);
+File.WriteAllText(@"C:\Users\marti\OneDrive\Desktop\New Text Document.txt", sb.ToString());
+
+Console.WriteLine("done");
